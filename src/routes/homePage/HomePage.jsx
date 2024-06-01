5c2de0773fec75d7bc36fa72db282d273c5541d2
hostem17 <vjjais17@gmail.com>
2024-06-01 15:28:45 +0530

    homepage design done

---
+++ src/routes/homePage/HomePage.jsx
@@ -0,0 +1,42 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Integer nec odio. Praesent libero. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+            Praesent libero.
+          </p>
+
+          <SearchBar />
+
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award gained</h2>
+            </div><div className="box">
+              <h1>2000+</h1>
+              <h2>Properties Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
