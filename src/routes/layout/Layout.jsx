54cc60db97e02e2f7d905d070f3ac7195c5252d0
hostem17 <vjjais17@gmail.com>
2024-06-01 21:15:26 +0530

    router added

---
+++ src/routes/layout/Layout.jsx
@@ -0,0 +1,21 @@
+import "./Layout.scss";
+
+import Navbar from "../../components/navbar/Navbar";
+
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/routes/listPage/ListPage.jsx
@@ -0,0 +1,7 @@
+import "./ListPage.scss";
+
+function ListPage() {
+  return <div>ListPage</div>;
+}
+
+export default ListPage;
