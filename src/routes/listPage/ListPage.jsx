ce3ac92ea86cac102286a550cb359d03d843b187
hostem17 <vjjais17@gmail.com>
2024-06-02 01:46:23 +0530

    map done

--- src/routes/listPage/ListPage.jsx
+++ src/routes/listPage/ListPage.jsx
@@ -1,5 +1,6 @@
 import Card from "../../components/Card/Card";
 import Filter from "../../components/filter/Filter";
+import Map from "../../components/map/Map";
 import { listData } from "../../lib/dummyData";
 import "./ListPage.scss";
 
@@ -16,7 +17,9 @@ function ListPage() {
           ))}
         </div>
       </div>
-      <div className="mapContainer">Hi</div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   );
 }
