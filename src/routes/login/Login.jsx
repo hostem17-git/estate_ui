f42727bfbbcafbad1327474c67baafddf81feedc
hostem17 <vjjais17@gmail.com>
2024-06-10 00:12:27 +0530

    register,login,logout done

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
 
 function Login() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    setError("");
+    setIsLoading(true);
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    try {
+      const response = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
+
+      localStorage.setItem("user", JSON.stringify(response.data));
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
+          <button disabled={isLoading}>Login</button>
+          {error && <span className="test">{error}</span>}
+
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,19 +1,55 @@
+import axios from "axios";
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    setError("");
+    e.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log(username, email, password);
+
+    try {
+      console.log("in");
+      const response = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      navigate("/login");
+    } catch (error) {
+      setError(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isLoading}>Register</button>
+          {error && <span className="test">{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
+
       <div className="imgContainer">
         <img src="/bg.png" alt="" />
       </div>
